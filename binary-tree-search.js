8850420695fc3f378bb89fbe5dcc10af7b3e4049
arifuzzaman.tanin <arifuzzaman.tanin@petronas.com>
2024-05-10 17:01:17 -0400

    Add binary tree search

--- binary-tree-search.js
+++ binary-tree-search.js
@@ -48,22 +48,52 @@ class BinaryTree{
             this.inorderTraversal(rootNode.right)
         }
     }
+    
+    search(value){
+        return this.searchNode(this.root, value)
+    }
+    
+    searchNode(rootNode, value){
+        if(rootNode === null){
+            return false
+        }
+        
+        if(rootNode.value > value){
+            return this.searchNode(rootNode.left, value)
+        }
+        else if(rootNode.value < value){
+            return this.searchNode(rootNode.right, value)
+        }
+        else{
+            return true
+        }
+    }
 }
 
 const tree = new BinaryTree();
 tree.insert(10);
 tree.insert(5);
 tree.insert(15);
+tree.insert(1);
 tree.insert(3);
 tree.insert(7);
+tree.insert(19);
+
 
 console.log("Inorder traversal of the binary tree:");
 tree.inorderTraversal(tree.root);
 
+console.log("Search for value 7:", tree.search(7)); // Should return true
+console.log("Search for value 20:", tree.search(20)); // Should return false
+
 // OUTPUT
-// Inorder traversal of the binary tree:
+// norder traversal of the binary tree:
+// 1
 // 3
 // 5
 // 7
 // 10
 // 15
+// 19
+// Search for value 7: true
+// Search for value 20: false
